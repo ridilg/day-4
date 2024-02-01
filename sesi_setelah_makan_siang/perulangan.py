0260394bba2aec3c4739227595c9a5167d41bba7
ridilg <siridi07@gmail.com>
2024-02-01 15:12:40 +0700

    push code to repo

---
+++ sesi_setelah_makan_siang/perulangan.py
@@ -0,0 +1,43 @@
+#s = 12
+
+#while s < 18:
+#    s += 1
+#    print (s)
+
+
+
+# s = 12
+# while s < 18:
+#     s+=1
+#     print (s)
+#     print ("selesai")
+
+
+# s=12
+# if s>18:
+#     s+=1
+# else:
+#     print(s)
+# s+=1;
+
+#dengan array
+s= ["12", "13", "14", "15"]
+for i in s:
+    print(i)
+    if i == 15:
+        break
+print ("selesai")
+
+
+s = 12
+while s > 0:
+    if s == 16:
+        break # Break Statemen
+    print(s)
+    s += 1
+while s > 15:
+    if s == 19:
+        break 
+    print('re Cek?: ', s)
+    s += 1
+print('Selesai')
